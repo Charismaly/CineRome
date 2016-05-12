868843e1731f73a0a09c8643c064c98139dae4f7
unknown <STAGIAIRE@R304-2.demostraining.com>
2016-05-12 16:30:50 +0200

    avec classe Prix

--- src/com/demos/cine/model/PP.java
+++ src/com/demos/cine/model/PP.java
@@ -4,12 +4,21 @@ public class PP {
 
 	public static void main(String[] args) {
 		
-		Article livre = new Article("ref1", "Martine à la plage",5.5, 25);
-		Article dvd = new Article("ref2", "Les aventures de Oui Oui", 15, 50);
-		
 		Panier p = new Panier();
-		p.ajouter(livre, 3);
-		p.ajouter(dvd, 2);
+		 
+		try {
+			Article livre = new Article("ref1", "Martine à la plage", new Prix(5.5), 25);
+			p.ajouter(livre, 3);
+		} catch (Exception e) {
+			System.out.println("Article non crée car prix incorrect");
+		}
+		
+		try {
+			Article dvd = new Article("ref2", "Les aventures de Oui Oui",new Prix(15), 50);
+			p.ajouter(dvd, 2);
+		} catch (Exception e) {
+			System.out.println("Article non crée car prix incorrect");
+		}
 		
 		System.out.println(p.toString());
 		System.out.println(p.getNbElements());
---
+++ src/com/demos/cine/model/Prix.java
@@ -0,0 +1,22 @@
+package com.demos.cine.model;
+
+public class Prix {
+	
+	private double valeur;
+	
+	public Prix(double p) throws Exception{
+		setPrix(p);
+	}
+	
+	public double getPrix(){
+		return valeur;
+	}
+	
+	public void setPrix(double p) throws Exception{
+		if (p<0){
+			 throw new Exception("Prix négatif");
+		 }
+		 valeur=p;
+	}
+
+}
